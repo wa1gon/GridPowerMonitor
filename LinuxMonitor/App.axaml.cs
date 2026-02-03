92798198407d4e280062fb4670ca74700db6d875
Darryl Wagoner <darryl@wagoner.me>
2026-02-02 19:19:24 -0600

    almost complete app initialization

--- LinuxMonitor/App.axaml.cs
+++ LinuxMonitor/App.axaml.cs
@@ -1,26 +1,26 @@
+.
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace LinuxMonitor
 {
     public partial class App : Application
     {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
 
-            public override void Initialize() => AvaloniaXamlLoader.Load(this);
-
-            public override void OnFrameworkInitializationCompleted()
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-                {
-                    var mainWindow = Program.AppHost.Services.GetRequiredService<MainWindow>();
-                    desktop.MainWindow = mainWindow;
-                }
-
-                base.OnFrameworkInitializationCompleted();
+                // set MainWindow or other startup logic
+                desktop.MainWindow = new MainWindow();
             }
-        
+
+            base.OnFrameworkInitializationCompleted();
+        }
     }
 }
